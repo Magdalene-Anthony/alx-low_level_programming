b8cd9a514fb8b22c05152d3dfe484253aa3bf82b
Magdalene-Anthony <effiongmagdalene90@gmail.com>
2022-12-16 19:56:04 -0800

    alx

---
+++ 0x04-more_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isdigit - function that checks for a digit (0 tbrough 9)
+ * @c: input
+ * Return: 1 if c is a digit, 0 otherwise
+ */
+int _isdigit(int c)
+{
+if (( >= 48) && (c <= 57))
+return (1);
+else
+return (0);
+}
---
+++ 0x04-more_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_most_numbers - function that print the numbers, from 0 to 9
+ *
+ * Return: 0-9 except 2 and 4
+ */
+void print_most_numbers(void)
+{
+int i;
+for (i =0; <= 9; i++)
+{
+if (( == 2) || ( 1 == 4))
+continue;
+
+else
+_putchar(i + '0');
+{
+_putchar('n');
+}
+	
